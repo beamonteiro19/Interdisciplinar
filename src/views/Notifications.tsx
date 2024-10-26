6a55df141f19a016ef63be300078b874b09b6739
Beatriz M. Vieira <99410946+beamonteiro19@users.noreply.github.com>
2024-10-26 18:56:31 -0300

    Tela de notificações

---
+++ src/views/Notifications.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Notifications = () => {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Bem-vindo à tela Notificações!</Text>
+            {/* Aqui você pode adicionar os componentes e estilo necessários conforme o modelo da imagem */}
+        </View>
+    );
+};
+
+export default Notifications;
